159e7a85f4469a211a9c134755ff17c09d7fa946
Raymond Wu <rayw.u17@gmail.com>
2018-09-15 02:48:11 -0400

    Winking

--- eyeanimations.js
+++ eyeanimations.js
@@ -1,4 +1,6 @@
-var blink = function(){
+
+var closeEyes= function( wink ){
+	var rand = Math.random()*2;
 	if(!animLocks.eyesAnimating){
 		animLocks.eyesAnimating = true;
 	var leftTrigger = {
@@ -8,8 +10,8 @@ var blink = function(){
 		trigger:false
 	};
 	var currypos = dot.LEyOffset;
+	if(!wink || rand <1){
 	var animlid = setInterval(function(){animateLeftEye(0,dot.LExOffset,1,currypos+29,0,dot.LExradius,-1,1, leftTrigger)},8);
-	var animrid = setInterval(function(){animateRightEye(0,dot.RExOffset,1,currypos+29,0,dot.RExradius,-1,1, rightTrigger)},8);
 	var triggerl = setInterval(function(){
 		if(leftTrigger.trigger){
 			clearInterval(triggerl);
@@ -26,23 +28,36 @@ var blink = function(){
 	},25);
 		}
 	},25);
+	}
+	if(!wink || rand >= 1){
+	var animrid = setInterval(function(){animateRightEye(0,dot.RExOffset,1,currypos+29,0,dot.RExradius,-1,1, rightTrigger)},8);
 	var triggerr = setInterval(function(){
 		if(rightTrigger.trigger){
 			clearInterval(triggerr);
 			clearInterval(animrid);
+			leftTrigger.trigger = false;
 			rightTrigger.trigger = false;
 			animrid = setInterval(function(){animateRightEye(0,dot.RExOffset,-1,currypos,0,dot.RExradius,1,30,rightTrigger)},8);
 			triggerr = setInterval(function(){
 		if(rightTrigger.trigger){
 			clearInterval(triggerr);
 			clearInterval(animrid);
+			animLocks.eyesAnimating = false;
 		}
 	},25);
 		}
 	},25);
+	}
+	
+	
 	
 	}
 	
 }
-
-dot.shape.addEventListener("click",blink);
+var blink = function(){
+	closeEyes(false);
+}
+var wink = function(){
+	closeEyes(true);
+}
+dot.shape.addEventListener("click",wink);
